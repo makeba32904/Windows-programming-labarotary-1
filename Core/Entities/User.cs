6fffdb656c483a37a2f3635e76782aa13e8794be
makeba <amka32904@example.com>
2026-03-16 14:55:38 +0800

    removed follow

--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -5,16 +5,10 @@ using System.Text;
 
 namespace Core.Entities
 {
-    public class User : BaseEntity, IFollowable
+    public class User : BaseEntity
     {
         public required string Username { get; set; }
         public required string Password { get; set; }
         public string Bio { get; set; } = "";
-        public List<Guid> Followers { get; set; } = new();
-        public List<Guid> Following { get; set; } = new();
-        public void Follow(Guid userId)
-        {
-            Following.Add(userId);
-        }
     }
 }
